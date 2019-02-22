3588451141fc8e8ba23517588bea11507a3626e0
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-02-22 22:41:07 +0700

    Add files via upload

---
+++ Free.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Lumayan Buat Pamer KeKwan Lu😂
+#Tools Ini Cuman Buat Iseng² doang
+
+echo "\033[0;31m]
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(  )/    -----
+   //////   * \/ *   ---
+  //// / // :    : ---
+ // /   /  /*    *--
+//          //..\\
+===========UU====UU==============
++          *//||\\*             +
++            ****               +
++ coded by : InYourG00D         +
++ Team : LNX#CREW S.T.C BUFT    +
++ kontak me : 6281382406878     +
++                               +
++  ^...^                 ^...^  +
++ <_* *_> TransferPulsa <_* *_> +
++   \_/                   \_/   +
+================================="
+
+echo "\033[0;32m]Masukan Nomer Anda: "
+read anda
+echo "\033[0;32m]Masukan Nomer Korban: "
+read korban
+echo "\033[0;32m]Jumblah Pulsa : "
+read pulsa
+echo
+echo "Wait"
+echo
+echo "$korban, Sukses Ngirim Pulsa $pulsa"
+echo "Done BosQ"
